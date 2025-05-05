95aefd5ddb7efe98e714dbfd1217fabf6f259dcf
cadusaboya <carlosepsaboya@gmail.com>
2025-05-05 14:14:10 -0400

    First migration test to PostgreSQL

--- erp-backend/erp_backend/settings.py
+++ erp-backend/erp_backend/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'erp_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'erpvo',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',           # empty string if there's no password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
