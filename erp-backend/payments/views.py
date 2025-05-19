dc917d979a75e8f732d0f008ad903426dcacdee3
cadusaboya <carlosepsaboya@gmail.com>
2025-05-19 08:36:14 -0300

    changed page size to match screen height

--- erp-backend/payments/views.py
+++ erp-backend/payments/views.py
@@ -40,7 +40,7 @@ def format_currency(value: Decimal) -> str:
     return locale.currency(value, grouping=True)
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 12
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
